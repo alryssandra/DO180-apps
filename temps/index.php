cf7708000af9db4752d248e53e601bde1b5af9ca
Student User <student@workstation.lab.example.com>
2022-08-04 09:32:48 -0400

    traduzindo o temps

--- temps/index.php
+++ temps/index.php
@@ -8,7 +8,7 @@
 
 <table>
 <tr>
-    <td>Enter the temperature to convert:</td>
+    <td>Entre o valor da temperatura para ser convertida:</td>
     <td><input type="text" name="temp" id="temp" size="10"></td>
 </tr>
 
